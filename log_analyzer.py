fd1b14fa215de0b8853911db449b59224bf0bf48
Zohrae <zahraelamssane@gmail.com>
2025-05-31 09:30:20 +0100

    Ajout des couleurs dans les logs

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,6 +1,9 @@
+from colorama import Fore, Style, init
+
 def analyze_logs(input_file='log.txt', output_file='rapport.txt'):
+    init(autoreset=True)  # Pour réinitialiser les couleurs automatiquement
     counts = {"INFO": 0, "WARNING": 0, "ERROR": 0}
-    
+
     with open(input_file, 'r') as file:
         for line in file:
             for level in counts:
@@ -11,7 +14,12 @@ def analyze_logs(input_file='log.txt', output_file='rapport.txt'):
         for level, count in counts.items():
             file.write(f"{level}: {count}\n")
 
-    print("Rapport généré dans rapport.txt")
+    # Affichage coloré dans le terminal
+    print(Fore.GREEN + f"INFO: {counts['INFO']}" + Style.RESET_ALL)
+    print(Fore.YELLOW + f"WARNING: {counts['WARNING']}" + Style.RESET_ALL)
+    print(Fore.RED + f"ERROR: {counts['ERROR']}" + Style.RESET_ALL)
+
+    print(Fore.CYAN + "✅ Rapport généré dans rapport.txt" + Style.RESET_ALL)
 
 if __name__ == "__main__":
     analyze_logs()
